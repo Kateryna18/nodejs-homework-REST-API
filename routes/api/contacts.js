ea103e1f9778d616a376d8a58231f531de70391a
Kateryna18 <mikolaichukkate@icloud.com>
2023-07-15 14:15:28 +0300

    done steps 4

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,77 +1,20 @@
 const express = require("express");
-
-const contacts = require("../../models/contacts.js");
-const { HttpError } = require("../../helpers");
-const { JoiSchema } = require("../../schemas");
-
 const router = express.Router();
 
+const ctrl = require("../../controllers/contacts");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contacts.getListContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+const { isValidId } = require("../../middlewares");
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    console.log(contactId);
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = JoiSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message)
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    console.log(error)
-    next(error);
-  }
-});
+router.get("/", ctrl.getAll);
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json({"message": "contact deleted"});
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = JoiSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message)
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", ctrl.addContact);
+
+router.delete("/:contactId", isValidId, ctrl.deleteContact);
+
+router.put("/:contactId", isValidId, JoiSchema.validate(req.body), ctrl.updateById);
+
 
 module.exports = router;
--- schemas/JoiSchema.js
+++ schemas/JoiSchema.js
@@ -4,6 +4,7 @@ const JoiSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().required(),
     phone: Joi.string().required(),
+    favorite: Joi.boolean(),
   })
 
 
