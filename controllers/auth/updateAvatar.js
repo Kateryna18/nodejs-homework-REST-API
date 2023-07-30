5cf7dd1b0cc7a83cff9601639c7da3ef5ae4a6de
Kateryna18 <mikolaichukkate@icloud.com>
2023-07-30 19:29:23 +0300

    corrected mistakes

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,24 +1,34 @@
 const path = require("path");
 const fs = require("fs/promises");
 const { User } = require("../../models/index");
+const Jimp = require("jimp");
+const { HttpError } = require("../../helpers");
 
-
-const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars")
+const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
-    const {_id} = req.user;
+  const { _id } = req.user;
 
-    const {path: tempUpload, originalname} = req.file;
-    const fileName = `${_id}_${originalname}`;
-    const resultUpload = path.join(avatarsDir, fileName);
-    await fs.rename(tempUpload, resultUpload)
+  const { path: tempUpload, originalname } = req.file;
+  const fileName = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, fileName);
+  await fs.rename(tempUpload, resultUpload);
 
-    const avatarURL = path.join("avatars", fileName);
-    await User.findByIdAndUpdate(_id, {avatarURL})
+  const avatarURL = path.join("avatars", fileName);
 
-    res.status(200).json({
-        avatarURL,
-      });
-}
+  try {
+    const image = await Jimp.read(resultUpload);
+    await image.resize(250, 250);
+    await image.writeAsync(resultUpload);
+  } catch (error) {
+    HttpError(500, "Avatar resizing error")
+  }
 
-module.exports = updateAvatar
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(200).json({
+    avatarURL,
+  });
+};
+
+module.exports = updateAvatar;
